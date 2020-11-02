4292293f51e96e8f7f63ceef691f3d1565be5202
imnotoriginal <imnotoriginal@users.noreply.github.com>
2020-11-02 18:40:40 +0300

    add useDebounce hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import React, { useState } from "react";
 import SearchInput from "./components/SearchInput";
 import SearchResults from "./components/SearchResults";
+import useDebounce from "./hooks/useDebounce";
 
 function App() {
   const [searchValue, setSearchValue] = useState<string>("");
+  const debouncedSearchValue = useDebounce(searchValue, 500);
 
   return (
     <>
       <SearchInput value={searchValue} onChange={setSearchValue} />
       <SearchResults data={[]} loading={false} />
+      {debouncedSearchValue}
     </>
   );
 }
---
+++ src/hooks/useDebounce.ts
@@ -0,0 +1,18 @@
+import { useState, useEffect } from "react";
+
+// source: https://habr.com/ru/post/492248/
+export default function useDebounce<T>(value: T, delay: number) {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [value]);
+
+  return debouncedValue;
+}
