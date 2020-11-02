0991acefd0a8b5c14a1660cc1d1837f799b2f613
imnotoriginal <imnotoriginal@users.noreply.github.com>
2020-11-02 19:42:20 +0300

    add error handling

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,24 @@ import useDebounce from "./hooks/useDebounce";
 
 function App() {
   const [searchValue, setSearchValue] = useState<string>("");
+  const [error, setError] = useState<string | null>(null);
   const [loading, setLoading] = useState<boolean>(false);
   const [data, setData] = useState<Array<Gif>>([]);
   const debouncedSearchValue = useDebounce<string>(searchValue, 1000);
 
   const uploadGifs = async () => {
     setLoading(true);
-    const gifs = await fetchGifs(debouncedSearchValue);
-    setData(gifs);
-    setLoading(false);
+    setError(null);
+    let gifs: Array<Gif> = [];
+
+    try {
+      gifs = await fetchGifs(debouncedSearchValue);
+    } catch (e) {
+      setError(e.message);
+    } finally {
+      setData(gifs);
+      setLoading(false);
+    }
   };
 
   useEffect(() => {
@@ -24,7 +33,7 @@ function App() {
   return (
     <>
       <SearchInput value={searchValue} onChange={setSearchValue} />
-      <SearchResults data={data} loading={loading} />
+      <SearchResults data={data} loading={loading} error={error} />
     </>
   );
 }
--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -5,13 +5,33 @@ import GifCardSkeleton from "./GifCardSkeleton";
 interface ISearchResultsProps {
   data: Array<Gif>;
   loading: boolean;
+  error: string | null;
 }
 
 const mockArray = new Array<null>(6).fill(null);
 type Results = React.ReactElement | Array<React.ReactElement>;
 
-const SearchResults: React.FC<ISearchResultsProps> = ({ data, loading }) => {
+const SearchResults: React.FC<ISearchResultsProps> = ({
+  data,
+  loading,
+  error,
+}) => {
   const results = useMemo<Results>(() => {
+    // An error occured
+    if (error)
+      return (
+        <>
+          <h2 className="text-center text-gray-500 text-lg w-full">Error:</h2>
+          <pre className="p-2 bg-blue-900 rounded text-gray-200 text-sm mx-auto my-2">
+            {error}
+          </pre>
+          <br />
+          <h2 className="text-center text-gray-500 text-lg w-full">
+            Do you know how to fix that?
+          </h2>
+        </>
+      );
+
     if (!loading) {
       if (data.length) {
         // We have results
