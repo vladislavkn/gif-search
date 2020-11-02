5da09b70c5f52aefaece01e563b604fa0f11dba7
imnotoriginal <imnotoriginal@users.noreply.github.com>
2020-11-02 19:24:02 +0300

    update styles

--- src/components/GifCard.tsx
+++ src/components/GifCard.tsx
@@ -7,9 +7,14 @@ interface IGifCardProps {
 
 const GifCard: React.FC<IGifCardProps> = ({ src, title }) => (
   <article className="p-2 xl:w-1/6 lg:w-1/5 md:w-1/4 sm:w-1/3 w-1/2">
-    <div className="bg-gray-300 rounded-lg p-2">
-      <img className="rounded" src={src} alt={`Gif: ${title}`} />
-      <div className="pt-2 text-lg text-center text-gray-600">{title}</div>
+    <div className="bg-gray-300 rounded-lg p-2 flex flex-col h-full">
+      <div
+        className="rounded w-full xl:h-56 lg:h-48 h-32 bg-cover bg-no-repeat bg-center"
+        style={{ backgroundImage: `url(${src})` }}
+      />
+      <div className="pt-2 flex flex-col flex-grow justify-center text-lg text-center text-gray-600">
+        {title}
+      </div>
     </div>
   </article>
 );
