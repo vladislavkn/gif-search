0eaf2df6e9f187ce654ab416deabd192570b08e9
Vladislavkn <inova136@gmail.com>
2020-11-09 11:32:33 +0300

    move api url to api/index

--- src/api/fetchGifs.ts
+++ src/api/fetchGifs.ts
@@ -1,4 +1,4 @@
-const API_URL: string = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.REACT_APP_API_KEY}&offset=0&rating=g&lang=en`;
+import { API_URL } from "./index";
 
 const mapRecords = (records: Array<any>): Array<Gif> =>
   records.map((record) => ({
---
+++ src/api/index.ts
@@ -0,0 +1,1 @@
+export const API_URL: string = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.REACT_APP_API_KEY}&offset=0&rating=g&lang=en`;
